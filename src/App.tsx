f898d7ca74b4371a9c5ab1ae16ac833427a6c2b9
asemFoqha <asem.sh.foqha@gmail.com>
2024-01-06 08:30:12 -0500

    adding a new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import LoginPage from "./Pages/LoginPage";
 import Navbar from "./components/Navbar/Navbar";
+import User from "./interfaces/User";
 
 function App() {
+  const [user, setUser] = useState<User | null>(null);
+
+  //#region Handlers
+
+  const handleLogin = (user: User) => {
+    console.log(user.name + " logedd in");
+  };
+
+  //#endregion
+
   return (
     <div className="App">
       <Navbar />
       <div className="full-height">
         {/* routes */}
-        <LoginPage />
+        <LoginPage handleLogin={handleLogin} />
       </div>
     </div>
   );
---
+++ src/components/Login/LoginForm.tsx
@@ -0,0 +1,58 @@
+import { FC } from "react";
+import User from "../../interfaces/User";
+
+interface Props {
+  handleLogin: (user: User) => void;
+}
+
+const LoginForm: FC<Props> = ({ handleLogin }) => {
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleLogin({ id: 123, name: "ayman" });
+        }}
+      >
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+export default LoginForm;
